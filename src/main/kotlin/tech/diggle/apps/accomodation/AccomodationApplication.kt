157f636bf7abe98a2bb5b0980bd26263907da8c8
Eddie G <eddie@diggle.tech>
2018-03-27 10:21:29 +0200

    Haa

--- src/main/kotlin/tech/diggle/apps/accomodation/AccomodationApplication.kt
+++ src/main/kotlin/tech/diggle/apps/accomodation/AccomodationApplication.kt
@@ -3,17 +3,16 @@ package tech.diggle.apps.accomodation
 //import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 
 //@Configuration
 //@ComponentScan
 //@EnableAutoConfiguration
 @SpringBootApplication
 class AccomodationApplication {
-    @Override
-    protected fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-        return application.sources(applicationClass)
-    }
+  //  @Override
+  //  protected fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+   //     return application.sources(applicationClass)
+   // }
 }
 
 
@@ -21,4 +20,4 @@ fun main(args: Array<String>) {
     SpringApplication.run(AccomodationApplication::class.java, *args)
 }
 
-val applicationClass = AccomodationApplication::class.java
+//val applicationClass = AccomodationApplication::class.java
